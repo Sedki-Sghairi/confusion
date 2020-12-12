da71ad445432a01ed5d49a9f7612680296b2ab68
Sedki-Sghairi <sedkisghairi3@gmail.com>
2020-12-12 16:31:51 +0400

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-
+import Menu from './components/MenuComponent';
 import { Navbar, NavbarBrand } from 'reactstrap';
 
 
@@ -11,6 +11,7 @@ function App() {
         <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
       </div>
     </Navbar>
+    <Menu/>
   </div>
   );
 }
