9b15dad4433df939e4eb1037dc61b38777b088bb
Sedki-Sghairi <sedkisghairi3@gmail.com>
2020-12-16 13:55:31 +0400

    react-router-part one

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home( props ) {
+    return (
+        <div>
+            <div className="container">
+                <h4>home</h4>
+            </div>
+        </div>
+    )
+}
